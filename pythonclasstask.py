87f34255da4c9bfbc6b6c75377dd1f43b65752d6
Patta <lokesh.patta@OURLOTTO.COM>
2026-03-31 13:00:12 +0530

    pythontasks

---
+++ pythonclasstask.py
@@ -0,0 +1,88 @@
+#1
+print(10)
+
+#2
+print(5,10,15)
+
+#3
+name = 'lokesh'
+print(name)
+
+#4
+firstname = "lokesh"
+lastname = "loki"
+print(firstname + lastname)
+
+#5
+age = 20
+print(f"I am {age} year old")
+
+#6
+name = "loki"
+city = "chennai"
+print("my name is {} and I live in {}".format(name,city))
+
+#7
+a=10
+b=20
+c=30
+print(a,b,c, sep='|')
+
+#8
+print("python & Rocks!")
+
+#9
+a=5
+b=8
+print(f"{a}+{b}", a+b)
+
+#10
+a = input("enter the name ")
+print("hello", a)
+
+#11
+city = 'chennai'
+print(city)
+
+#12
+a = 10
+b = 20
+a,b = b,a
+print(a,b)
+
+#13
+a,b,c= 5,10,"python"
+print(a,b,c)
+
+#14
+color1, color2, color3 = "RED"
+print(color1,color2,color3)
+
+#15
+value =3.14
+print(type(value))
+
+#16
+name = "lokesh"
+age = 18
+print("my name is {} and I am {} years old".format(name,age))
+
+#17
+a="15"
+b=3
+print(a*b)
+
+#18
+is_python_func = True
+print(is_python_func)
+
+#19
+x=10
+x +=15 #x= x+15 = 25
+print(x)
+
+#20
+a,b,c = 5,5,5
+b=10
+print(a,b,c)
+
